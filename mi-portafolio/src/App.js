0db2391ffe7f4f91fee3f5dc00010e46e0cd756c
anamonsalve <anamonsalve96@gmail.com>
2024-02-26 17:22:21 -0500

    create app react for portfolio

---
+++ mi-portafolio/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+          <a href="#cv">See My CV</a>
+        </nav>
+        <main>
+          <h1>I Merge Art And Technology To Create Cutting-Edge Websites</h1>
+          <p>Crafting digital masterpieces that redefine online experiences</p>
+          <button>See My CV</button>
+        </main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
